702458f59bec9d31d3d7037b9c22c8b0cfec1632
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-09-28 23:40:29 +0200

    Add user is functional.

--- base.py
+++ base.py
@@ -2,11 +2,17 @@ import sqlite3
 from flask import Flask, request, session, g, redirect, url_for, \
      abort, render_template, flash
 from contextlib import closing
-from database import init_db, connect_db, get_db, before_request, \
-     close_connection
+from models import db, User
+from forms import AddUserForm
+
+
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+app.config.from_object('config')
+with app.app_context():
+	db.init_app(app)
+	db.create_all()
 
 @app.route('/')
 def show_home():
@@ -16,6 +22,20 @@ def show_home():
 def show_about():
     return render_template('about.html')
 
+@app.route('/admin')
+def show_admin():
+	return render_template('admin.html')
+
+@app.route('/adduser', methods=['GET', 'POST'])
+def add_user():
+    form = AddUserForm(request.form)
+    if request.method == 'POST' and form.validate():
+        user = User(form.username.data, form.email.data,
+                    form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect('/admin')
+    return render_template('adduser.html', form=form, users=User.query.all())
 
 if __name__ == '__main__':
     app.run(debug=True)
