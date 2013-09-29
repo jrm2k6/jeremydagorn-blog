86fa9cea6c8eefdb3aead4533b423373944278a1
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2013-09-29 23:07:29 +0200

    [models] store hashed password.

--- models.py
+++ models.py
@@ -13,7 +13,13 @@ class User(db.Model):
 	def __init__(self, username, email, password):
 		self.username = username
 		self.email = email
-		self.password = password
+		self.password = self.set_password(password)
+
+	def set_password(self, password):
+		return generate_password_hash(password)
+
+	def check_password(self, password):
+		return check_password_hash(self.pw_hash, password)
 
 	def __repr__(self):
 		return '<User %r>' % self.username
