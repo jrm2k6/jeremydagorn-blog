2553950467d80463d0eba4ccb9a918139d96c0f7
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2014-03-07 15:31:01 -0800

    [tests] add first test checking insertion in database.

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -4,8 +4,10 @@ from flask import Flask
 from publishr import base
 from publishr.models import db
 from . import PublisherAppTestCase
-from utils import FlaskTestAuthenticationUtils
+from utils import FlaskTestAuthenticationUtils, FlaskTestModelUtils
 import unittest
+import base64
+
 
 
 class AdminAuthenticationTest(PublisherAppTestCase, TestCase, FlaskTestAuthenticationUtils):
@@ -20,6 +22,17 @@ class AdminAuthenticationTest(PublisherAppTestCase, TestCase, FlaskTestAuthentic
     def test_show_admin_with_incorrect_password_returns_401(self):
         res = self.login_with_credentials_request("admin", "username_test", "wrong_secret")
         self.assert401(res)
+
+class AdminAddItemsTest(PublisherAppTestCase, TestCase, FlaskTestAuthenticationUtils, FlaskTestModelUtils):
+    def test_add_category_in_database(self):
+        new_user_username =  "username_test"
+        res = self.get_auth_required_page_with_post_data(
+                                url='adduser', 
+                                data_dict=dict(username=new_user_username, email="test@email.com", password="random"),
+                                username="username_test",
+                                password="secret_test")
+
+        self.assert_user_with_username_exists_in_database(new_user_username)
 
 
 if __name__ == "__main__":
--- tests/utils.py
+++ tests/utils.py
@@ -1,3 +1,5 @@
+from publishr.models import User
+from unittest import TestCase
 import base64
 
 class FlaskTestAuthenticationUtils(object):
@@ -9,4 +11,17 @@ class FlaskTestAuthenticationUtils(object):
             })
 
 		return res
-	
+
+	def get_auth_required_page_with_post_data(self, url, data_dict, username, password):
+		res = self.client.post(
+				url, 
+				data=data_dict,
+        		headers={'Authorization': 'Basic ' + base64.b64encode(username + ":" \
+        			+ password)
+            })
+
+		return res
+
+class FlaskTestModelUtils(TestCase, object):
+	def assert_user_with_username_exists_in_database(self, _username):
+		self.assertTrue(User.query.filter_by(username=_username).first() is not None)
