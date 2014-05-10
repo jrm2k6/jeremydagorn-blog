92cffc3e60ed8bb32cb92f8aee4faefa1829de4d
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2014-05-10 20:24:39 +0200

    [test] add test when database is empty

--- tests/test_admin_delete_resources.py
+++ tests/test_admin_delete_resources.py
@@ -4,12 +4,13 @@ from flask import Flask
 from publishr import base
 from publishr.models import db
 from . import PublisherAppTestCase
-from utils import FlaskTestAuthenticationUtils, FlaskTestModelUtils
+from utils import FlaskTestAuthenticationUtils, FlaskTestModelUtils, AssertErrorCode
 import unittest
 import base64
 
 
-class AdminDeleteItemsTest(PublisherAppTestCase, TestCase, FlaskTestAuthenticationUtils, FlaskTestModelUtils):
+class AdminDeleteItemsTest(PublisherAppTestCase, TestCase, FlaskTestAuthenticationUtils, 
+	FlaskTestModelUtils, AssertErrorCode):
 	# no need to do it for all models, the behavior is the same
     def test_delete_user_in_database_with_existing_item(self):
     	username = 'my_user'
@@ -23,6 +24,16 @@ class AdminDeleteItemsTest(PublisherAppTestCase, TestCase, FlaskTestAuthenticati
                                 password="secret_test")
 
         self.assert_user_with_username_exists_in_database_is_false(username)
+        self.assert200(res)
+
+    def test_delete_user_in_database_with_empty_model(self):
+        res = self.get_auth_required_page_with_post_data(
+                                url='/delete/user/1', 
+                                data_dict=dict(),
+                                username="username_test",
+                                password="secret_test")
+
+        self.assert410(res)
 
 
 if __name__ == "__main__":
