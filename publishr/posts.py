889a9678c73fc32559b9152105387cfc3560c2c8
Jeremy Dagorn <jeremy.dagorn@prezi.com>
2014-05-13 18:54:23 +0200

    add todo

--- publishr/posts.py
+++ publishr/posts.py
@@ -19,6 +19,7 @@ class PreviewPost(object):
 
 def load_blogpost(file_to_load):
 	file_to_read = os.getcwd() + '/' + file_to_load
+	#TODO: add try catch here
 	with open(file_to_read, 'r') as f:
 		content = ''.join(f.readlines())
 	return content
