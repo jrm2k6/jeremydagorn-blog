5c5b29859394d8dbb542ef92a977f9baac7368ce
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2014-10-07 16:54:04 -0700

    do not track config.py in tests folder

--- publishr/tests/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-SECRET = 'secret_test'
-USERNAME = 'username_test'
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:////mnt/ramdisk/testdb.db'
-PATH_POSTS_FOLDER = '/publishr/tests/posts/'
-PATH_CONTENT_FOLDER = '/publishr/tests/content/'
-UPLOAD_FOLDER = 'publishr/tests/uploads'
