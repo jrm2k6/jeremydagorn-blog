3948b18dba252e9646370a255504b3378dd200ca
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-04-19 09:53:39 -0700

    install node/npm + symlink

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install python-pip python-dev nginx virtualenv libxml2-dev libxslt1-dev zlib1g-dev lxml
+sudo apt-get install python-pip python-dev nginx virtualenv libxml2-dev libxslt1-dev zlib1g-dev lxml uwsgi nodejs npm
+sudo npm install -g less
+sudo ln -s /usr/bin/nodejs /usr/bin/node
