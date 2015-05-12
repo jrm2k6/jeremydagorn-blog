f64513726af831ca20e8f97b5031a672989cdb68
Jeremy Dagorn <jeremy.dagorn@gmail.com>
2015-05-12 08:49:32 -0700

    modify setup_machine.sh

--- scripts/setup_machine.sh
+++ scripts/setup_machine.sh
@@ -4,3 +4,7 @@ sudo apt-get update
 sudo apt-get install python-pip python-dev nginx virtualenv libxml2-dev libxslt1-dev zlib1g-dev lxml uwsgi nodejs npm
 sudo npm install -g less
 sudo ln -s /usr/bin/nodejs /usr/bin/node
+sudo apt-get install python-pip python-dev nginx
+
+sudo pip install virtualenv
+pip install uwsgi flask
